a77e989da5e8ca18ea4bcc8e25c568d1bc1adc56
Md Sahil Achanoor <117533136+Mahmadsahil@users.noreply.github.com>
2024-09-19 07:30:03 -0800

    Update NewsPage.jsx

--- src/Component/NewsPage.jsx
+++ src/Component/NewsPage.jsx
@@ -1,23 +1,27 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import NewsList from "./NewsList";
 import { lazy, Suspense, useEffect, useState } from "react";
 import NewsBoxShimmer from "../Shimmer/NewsBoxShimmer";
 import NewsListShimmer from "../Shimmer/NewsListShimmer";
 import MockData from "../Utils/MockData.json";
+import { addMockData } from "../store/appSlice";
 const NewsBox = lazy(() => import("./NewsBox"));
 
 const NewsPage = () => {
   const showNews = useSelector((state) => state.app.showNews);
+  const MockData2 = useSelector((state) => state.app.MockData2);
+  const dispatch = useDispatch();
   const [articles, setArticles] = useState(MockData);
-  const numbers = [0, 1, 2, 3, 4, 5];
 
   useEffect(() => {
     const getAPIData = async () => {
       try {
         const response = await fetch(
-          " https://newsapi.org/v2/top-headlines?country=us&apiKey=d5777bb7675a4338820bd1b99566b8d2"
+          "https://newsapi.org/v2/top-headlines?country=us&apiKey=d5777bb7675a4338820bd1b99566b8d2"
         );
         const data = await response.json();
+        dispatch(addMockData(data.articles));
+        console.log("MockData2", MockData2);
 
         setArticles(data.articles);
       } catch (error) {
@@ -27,20 +31,21 @@ const NewsPage = () => {
 
     getAPIData();
   }, []);
+  console.log("MockData1", MockData2);
 
   return (
     <div className="h-full w-full  md:w-9/12 flex flex-wrap justify-center p-8 md:p-4 gap-4 bg-blue-50 overflow-y-scroll">
-      {MockData.map((data, idx) =>
-          !showNews ? (
-            <Suspense key={idx} fallback={<NewsBoxShimmer />}>
-              <NewsBox data={data} />
-            </Suspense>
-          ) : (
-            <Suspense key={idx} fallback={<NewsListShimmer />}>
-              <NewsList data={data} />
-            </Suspense>
-          )
-        )}
+      {MockData2.map((data, idx) =>
+        !showNews ? (
+          <Suspense key={idx} fallback={<NewsBoxShimmer />}>
+            <NewsBox data={data} />
+          </Suspense>
+        ) : (
+          <Suspense key={idx} fallback={<NewsListShimmer />}>
+            <NewsList data={data} />
+          </Suspense>
+        )
+      )}
     </div>
   );
 };
