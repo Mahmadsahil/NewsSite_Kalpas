a60962e3d5b360819a58cd908593ca1aeb23032a
Md Sahil Achanoor <117533136+Mahmadsahil@users.noreply.github.com>
2024-09-19 07:29:31 -0800

    Update NewsBox.jsx

--- src/Component/NewsBox.jsx
+++ src/Component/NewsBox.jsx
@@ -1,5 +1,5 @@
 import { IoMdClose } from "react-icons/io";
-import { addNewsData, showNewsData } from "../store/appSlice";
+import { addNewsData, removeMockData, showNewsData } from "../store/appSlice";
 import { useDispatch } from "react-redux";
 
 const NewsBox = ({ data }) => {
@@ -11,13 +11,25 @@ const NewsBox = ({ data }) => {
     dispatch(addNewsData(data));
     console.log(data);
   };
+
+  const handleCancleNews = (data) => {
+    // dispatch(addNewsData(data));
+    dispatch(removeMockData(data));
+  };
+
   return (
     <>
       <div
-        onClick={()=>handleNewsData(data)}
+        onClick={() => handleNewsData(data)}
         className="w-full md:w-60 bg-white p-4 flex flex-col items-center gap-2 rounded relative cursor-pointer"
       >
-        <IoMdClose className="text-xl text-red-600 absolute top-0 right-0 m-2 cursor-pointer" />
+        <IoMdClose
+          onClick={(e) => {
+            e.stopPropagation();
+            handleCancleNews(data);
+          }}
+          className="text-xl text-red-600 absolute top-0 right-0 m-2 cursor-pointer"
+        />
         <h1 className="text-sm font-semibold">{title}</h1>
         <p className="">{publishedAt}</p>
         <img className="w-full" src={urlToImage} alt=""></img>
